6bf06dfaae296ab2e944877a6fcc8f1540a050ca
Daki15 <darjanpaunovic@gmail.com>
2022-01-07 10:01:38 +0100

    End of modification

--- Fibonacci.c
+++ Fibonacci.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int *fibonacciCache;
+
 int fibonacci_iter(int number){
     int n2, n0=0, n1=1;
     for(int i=1; i<number; i++){
@@ -12,8 +14,18 @@ int fibonacci_iter(int number){
 }
 
 int fibonacci_rec(int number){
-    if(number <= 1) return number;
-    return fibonacci_rec(number - 1) + fibonacci_rec(number - 2);
+    if(number <= 1) {
+        return number;
+    }
+
+    if(fibonacciCache[number] != 0){
+        return fibonacciCache[number];
+    }
+
+    int nthFibonacciNumber = ( fibonacci_rec(number - 1) + fibonacci_rec(number - 2) );
+    fibonacciCache[number] = nthFibonacciNumber;
+
+    return nthFibonacciNumber;
 }
 
 int main(){
@@ -21,10 +33,14 @@ int main(){
     
     printf("Enter the number:\n> ");
     scanf("%d", &number);
+    
+    fibonacciCache = (int*)calloc(number + 1, sizeof(int));
 
-    printf("The %d-th number is %d", number, fibonacci_iter(number));
+    printf("\nThe %d-th number is %d", number, fibonacci_iter(number));
 
     printf("\nThe %d-th number from recursiv calcul is %d", number, fibonacci_rec(number));
+
+    free(fibonacciCache);
 
     return 0;
 }
